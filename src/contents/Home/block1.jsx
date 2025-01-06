4fe7a993e5da73fe1e8dbd7ef30c26e3ca6189a4
Nitish847235 <nitishpatwarabihar@gmail.com>
2025-01-06 13:34:27 +0530

    folder structure

---
+++ src/contents/Home/block1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Block = () => {
+  return (
+    <div>block1</div>
+  )
+}
+
+export default Block
---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Block from '../../contents/Home/block1'
+
+const home = () => {
+  return (
+    <div>
+        <Block />
+        
+    </div>
+  )
+}
+
+export default home
